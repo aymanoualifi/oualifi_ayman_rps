bcfc26dbad3f3f844a4a628ac6f388cbff3ca80d
aymanoualifi <oualifiayman9@gmail.com>
2021-11-12 17:27:38 -0800

    adding gamecomparison

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 from random import randint
-from gamecomponents import winlose, gameVars
+from gamecomponents import winlose, gameVars, gamecomparison
 
 # set up our game loop so that we can keep playing and not exit
+#define a winlose function and invoke it
+#when lives run out computer or player
+
 while gameVars.player is False:
     gameVars.player = input("Choose your weapon: rock, paper or scissors: ")
     gameVars.computer = gameVars.choices[randint(0, 2)]
 
     print("player chose: " + gameVars.player)
     print("computer chose: " + gameVars.computer)
-
-    if gameVars.computer == gameVars.player:
-        # tie - nothing else to compare, so it'll exit
-        print("tie! try again")
-
-    elif gameVars.player == "rock":
-        if gameVars.computer == "paper":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
-
-    elif gameVars.player == "paper":
-        if gameVars.computer == "scissors":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
-
-    elif gameVars.player == "scissors":
-        if gameVars.computer == "rock":
-            print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
-        else:
-            print("you win!")
-            gameVars.computerLives = gameVars.computerLives - 1
-
+  
     print("player life count: " + str(gameVars.playerLives))
     print("computer life count: " + str(gameVars.computerLives))
 
