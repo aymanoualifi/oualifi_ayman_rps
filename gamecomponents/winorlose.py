d898b46e63f1d906908016da6759031cadcfb91a
aymanoualifi <oualifiayman9@gmail.com>
2021-11-12 17:01:45 -0800

    made changes

---
+++ gamecomponents/winorlose.py
@@ -0,0 +1,15 @@
+from gameComponents import gameVars
+
+
+def winorlose(status):
+    print("you " + status)
+
+    choice = input("do you want to play again? y/n: ")
+
+    if choice == "n":
+        print("========= hard luck! ==========")
+        exit()
+    elif choice == "y":
+        gameVars.playerLives = 2
+        gameVars.computerLives = 2
+        gameVars.player = False 
